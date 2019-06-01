30eeac5d089c8a9beb6a6cd164ccfe8e83b5b180
Artem Buslov <buslov.artem@gmail.com>
2019-06-01 13:22:48 +0300

    Initial version

---
+++ fakewinlirc.py
@@ -0,0 +1,60 @@
+import os.path
+import socket
+import threading
+import bottle
+
+clients = []
+
+
+def sendtoall(cmd):
+    s = "{c} {n:02x} {s} python\n".format(c="0" * 16, n=0, s=cmd)
+    d = bytes(s, encoding='ascii')
+    closed = []
+    for c in clients:
+        try:
+            c.sendall(d)
+        except socket.error as e:
+            print("exception:", e.errno, e)
+            closed.append(c)
+    for c in closed:
+        c.close()
+        clients.remove(c)
+
+
+@bottle.route("/")
+def index():
+    return bottle.static_file("index.htm", root='')
+
+
+@bottle.route("/static/<path>")
+def staticfiles(path):
+    return bottle.static_file(path, root='static')
+
+
+@bottle.post("/ajax")
+def ajax():
+    cmd = bottle.request.forms.get('cmd')
+    sendtoall(cmd)
+
+
+def tcpserver():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("0.0.0.0", 8765))
+    s.listen(5)
+    while True:
+        c, a = s.accept()
+        print("accepted", a)
+        clients.append(c)
+
+
+if __name__ == "__main__":
+    print("os.getcwd():", os.getcwd())
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    print("scriptdir:", scriptdir)
+    os.chdir(scriptdir)
+    print("os.getcwd():", os.getcwd())
+    t = threading.Thread(target=tcpserver)
+    t.daemon = True
+    t.start()
+    bottle.debug(True)
+    bottle.run(host="0.0.0.0", port=8080)
